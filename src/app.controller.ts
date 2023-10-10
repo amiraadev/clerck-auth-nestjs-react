629ad8cd095e859b3551a85013f48ef386855088
amiraallagui <allagui_fawzi@outlook.fr>
2023-10-10 15:35:03 +0100

    get reports by type

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
-
-@Controller('report/income')
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { ReportType, data } from 'src/data';
+@Controller('report/:type')
 export class AppController {
   @Get()
-  getAllIncomeReports() {
-    return [];
+  getAllIncomeReports(@Param('type') type: string) {
+    // console.log(type);
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
   @Get(':hi')
   getIncomeReportById() {
--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,35 @@
 /* eslint-disable prettier/prettier */
+export enum ReportType {
+    INCOME = "income",
+    EXPENSE = "expense"
+}
 export const data: Data =  { 
-    report: []
+    report: [
+        { 
+         id: "uid1",
+        source: "salary",
+        amount: 7500,
+        created_at: new Date(),
+        updated_at: new Date(),
+        type:ReportType.INCOME
+    },
+        { 
+         id: "uid2",
+        source: "youtube",
+        amount: 2500,
+        created_at: new Date(),
+        updated_at: new Date(),
+        type:ReportType.INCOME
+    },
+        { 
+         id: "uid3",
+        source: "food",
+        amount: 2500,
+        created_at: new Date(),
+        updated_at: new Date(),
+        type:ReportType.EXPENSE
+    },
+   ]
  }
 
 interface Data {
@@ -14,7 +43,3 @@ interface Data {
     }[]
 }
 
-enum ReportType {
-    INCOME = "income",
-    EXPENSE = "expense"
-}
