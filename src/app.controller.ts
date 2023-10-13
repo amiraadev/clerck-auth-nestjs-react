53c33e3d851bfef4c05cccc2f64f9ad3229cd79c
amiraallagui <allagui_fawzi@outlook.fr>
2023-10-13 10:50:37 +0100

    HttpCode decorator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Param,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
 import { ReportType, data } from 'src/data';
+import { v4 as uuid } from 'uuid';
 @Controller('report/:type')
 export class AppController {
   @Get()
@@ -18,15 +28,47 @@ export class AppController {
       .find((report) => report.id === id);
   }
   @Post()
-  createReport() {
-    return 'Created';
+  createReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: string,
+  ) {
+    const newReport = {
+      id: uuid(),
+      amount,
+      source,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+    };
+    data.report.push(newReport);
+    return newReport;
   }
-  @Put()
-  updateReport() {
-    return 'Updated';
+  @Put(':id')
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { amount: number; source: string },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const reportToUpdate = data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+    if (!reportToUpdate) return;
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+    };
+    return data.report[reportIndex];
   }
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport() {
-    return 'Deleted';
+  deleteReport(@Param('id') id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+    if (reportIndex === -1) return;
+    data.report.splice(reportIndex, 1);
   }
 }
